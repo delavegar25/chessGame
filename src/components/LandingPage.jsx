6a896c0687cf1c1a34c1fe62d3373ad89a2eed00
pascal <pascalokereke18@gmail.com>
2024-08-09 12:33:19 +0100

    landing page update

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -9,9 +9,33 @@ const LandingPage = () => {
         <div className='bg-cover bg-center min-h-screen min-w-screen flex flex-col justify-start items-center' 
         style={{ backgroundImage: `url(${PawnImage})`}}>
             <div className='w-full p-4 bg-opacity-50 justify-between items-center'>   
-            <h1 className='text-left text-3xl font-bold text-black'>
+            <h1 className='text-left text-xl font-bold text-black'>
                 CHESS MATCH 
                 </h1>
+            <div className='absolute 
+            right-60 top-4'>
+                <Link to='/signin'>
+                  <button className='
+                  text-white font-bold py-2
+                  px-4 
+                  relative
+                  left-52
+                  rounded'>
+                     Sign in 
+                  </button>
+                </Link>
+                <Link to='/signup'>
+                   <button className='bg-gray-600
+                   text-white 
+                   font-bold
+                   relative
+                   left-52
+                   py-2
+                   px-4 rounded-lg'>
+                     Sign Up 
+                   </button>
+                </Link>
+            </div>
         
         </div>
         </div>
